69de597700e69a94444b78b429427558197b87cc
Thomas Walpole <twalpole@gmail.com>
2018-03-02 11:18:13 -0800

    Play with idea of plugins to support actions with common UI libraries

--- lib/capybara.rb
+++ lib/capybara.rb
@@ -133,6 +133,10 @@ module Capybara
       servers[name.to_sym] = block
     end
 
+    def register_plugin(name, plugin)
+      plugins[name.to_sym] = plugin
+    end
+
     ##
     #
     # Add a new selector to Capybara. Selectors can be used by various methods in Capybara
@@ -189,6 +193,10 @@ module Capybara
 
     def servers
       @servers ||= {}
+    end
+
+    def plugins
+      @plugins ||= {}
     end
 
     # Wraps the given string, which should contain an HTML document or fragment
